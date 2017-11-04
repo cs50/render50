b1f817f8415dbb8ba4200f5de1d326fbcf890e4d
David J. Malan <malan@harvard.edu>
2017-11-04 17:52:07 -0400

    added side by side but debugging

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is render50, with which you can render source code as PDFs.",
-    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "natsort", "Pygments", "six>=1.10.0", "termcolor", "WeasyPrint"],
+    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "natsort", "Pygments", "PyPDF2", "six>=1.10.0", "termcolor", "WeasyPrint"],
     keywords=["render", "render50"],
     name="render50",
     scripts=["render50"],
     url="https://github.com/cs50/render50",
-    version="2.0.1"
+    version="2.1.0"
 )
