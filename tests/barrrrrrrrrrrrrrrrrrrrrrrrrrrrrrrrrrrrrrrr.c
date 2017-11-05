9c586ee503cc2c6524bcb2161af459126627da03
David J. Malan <malan@harvard.edu>
2017-11-04 23:34:38 -0400

    removed tests

--- tests/barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("bar"); // this is a long comment, this is a long comment, this is a long comment, this is a long comment, this is a long comment, this is a long comment, this is a long comment, this is a long comment, this is a long comment, this is a long comment
-}
