7d880cdabc12169132f1ba804911748e3eece26f
David J. Malan <malan@harvard.edu>
2020-09-29 13:08:00 -0400

    added support for expanding braces before globbing

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is render50, with which you can render source code as PDFs.",
-    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "bs4", "natsort", "Pygments", "PyPDF2", "requests", "six>=1.10.0", "termcolor", "WeasyPrint>=51"],
+    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "braceexpand", "bs4", "natsort", "Pygments", "PyPDF2", "requests", "six>=1.10.0", "termcolor", "WeasyPrint>=51"],
     keywords=["render", "render50"],
     name="render50",
     scripts=["render50"],
     url="https://github.com/cs50/render50",
-    version="4.0.3"
+    version="4.1.0"
 )
