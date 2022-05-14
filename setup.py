3dd45070c667fad412273ce6c5992ba1476e3de6
David J. Malan <malan@harvard.edu>
2022-05-14 01:46:47 -0400

    updated weasyprint for emoji, fixes #80 assuming fonts-noto-color-emoji installed

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is render50, with which you can render source code as PDFs.",
-    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "braceexpand", "beautifulsoup4", "natsort", "Pygments>=2.7.1", "PyPDF2", "requests", "six>=1.10.0", "termcolor", "WeasyPrint==52.5"],
+    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "braceexpand", "beautifulsoup4", "natsort", "Pygments>=2.7.1", "PyPDF2", "requests", "six>=1.10.0", "termcolor", "WeasyPrint==55.0"],
     keywords=["render", "render50"],
     name="render50",
     scripts=["render50"],
     url="https://github.com/cs50/render50",
-    version="7.0.1"
+    version="7.0.2"
 )
