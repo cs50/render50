663dcca0fe2ec29f4d1f76d236dabdbc61cb1f96
Rongxin Liu <rongxinliu.dev@gmail.com>
2023-04-21 10:07:49 -0400

    fixated PyPDF2 version for now

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is render50, with which you can render source code as PDFs.",
-    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "braceexpand", "beautifulsoup4", "natsort", "Pygments>=2.7.1", "PyPDF2", "requests", "six>=1.10.0", "termcolor", "WeasyPrint"],
+    install_requires=["backports.shutil_get_terminal_size", "backports.shutil_which", "braceexpand", "beautifulsoup4", "natsort", "Pygments>=2.7.1", "PyPDF2==2.12.1", "requests", "six>=1.10.0", "termcolor", "WeasyPrint"],
     keywords=["render", "render50"],
     license="GPLv3",
     long_description_content_type="text/markdown",
@@ -18,5 +18,5 @@ setup(
     python_requires=">=3.6",
     scripts=["render50"],
     url="https://github.com/cs50/render50",
-    version="9.1.1"
+    version="9.1.2"
 )
